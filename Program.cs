204fc03702b4a976565350568e1498c9098590ba
ozlemyayalan <ozlem.yayalan@gmail.com>
2021-02-10 00:17:29 +0300

    Add project files.

--- Program.cs
+++ Program.cs
@@ -11,17 +11,12 @@ namespace GameProject
         {
             /*
              * Gereksinimler
-
             1.Oyuncuların sisteme kayıt olabileceği, bilgilerini güncelleyebileceği, kayıtlarını silebileceği bir ortamı simule ediniz.Müşteri bilgilerinin doğruluğunu e-devlet sistemlerini kullanarak doğrulama yapmak istiyoruz. (E - devlet sistemlerinde doğrulama TcNo, Ad, Soyad, DoğumYılı bilgileriyle yapılır.Bunu yapacak servisi simule etmeniz yeterlidir.)
-
             2.Oyun satışı yapılabilecek satış ortamını simule ediniz.(Yapılan satışlar oyuncu ile ilişkilendirilmelidir. Oyuncunun parametre olarak metotta olmasını kastediyorum.)
-
             3.Sisteme yeni kampanya girişi, kampanyanın silinmesi ve güncellenmesi imkanlarını simule ediniz.
-
             4.Satışlarda kampanya entegrasyonunu simule ediniz.
-            
              */
-
+            
 
 
             BasePlayerManager playerManager = new PlayerManager(new MernisServiceAdapter());
@@ -41,10 +36,7 @@ namespace GameProject
             campaignManager.UpdateCampaign(new Entities.Campaign { CampaignName = "Valentines day" });
 
 
-
-
             Console.WriteLine("--------------");
-
 
 
             OrdersManager ordersManager = new OrdersManager();
@@ -52,12 +44,7 @@ namespace GameProject
                 new Entities.Game { GameId = 216, GameName = "Neverwinter Nights" },
                 new Entities.Player { DateOfBirth = 1169, FirstName = "Feanor", LastName = "High King of the Ñoldor", NationalityId = 10000000146 },
                 new Entities.Orders { OrderId = 3515413 },
-                new Entities.Campaign { CampaignId = 1, CampaignName = "Gamer Nights", CampaignDiscountRate = 0.2 }
-
-                );
-
-
-
+                new Entities.Campaign { CampaignId = 1, CampaignName = "Gamer Nights", CampaignDiscountRate = 0.2 });
 
 
             Console.ReadLine();
